66afcc6958e7ca43223f0016cc601bf59bddc739
chinu99380955899 <sutarchinmaya385@gmail.com>
2024-06-26 00:35:07 +0530

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,102 @@
+import { Box, Container, Heading, Image, Stack, Text } from '@chakra-ui/react';
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import img1 from "../assets/1.jpg";
+import img2 from "../assets/2.jpg";
+import img3 from "../assets/3.jpg";
+import img4 from "../assets/4.jpg";
+import img5 from "../assets/5.png";
+
+const headingOptions = {
+  pos: 'absolute',
+  left: '50%',
+  top: '50%',
+  transform: 'translate(-50%,-50%)',
+  textTransform: 'uppercase',
+  p: '4',
+  size: '4xl',
+};
+
+const Home = () => {
+  return (
+    <Box> 
+      <MyCarousel />
+
+      <Container maxW={'container.xl'} minH={'100vh'} p="16">
+        <Heading
+          textTransform={'uppercase'}
+          py="2"
+          w={'fit-content'}
+          borderBottom={'2px solid'}
+          m="auto"
+        >
+          Services
+        </Heading>
+
+        <Stack
+          h="full"
+          p={'4'}
+          alignItems={'center'}
+          direction={['column', 'row']}
+        >
+          <Image src={img5} h={['40', '400']} filter={'hue-rotate(-130deg)'} />
+
+          <Text
+            letterSpacing={'widest'}
+            lineHeight={'190%'}
+            p={['4', '16']}
+            textAlign={'center'}
+          >
+        <h1>Meet Chinmaya Sutar: A Promising React Developer
+        Introduction</h1> <br />  
+         <h4>Hello, world! My name is Chinmaya Sutar, and I recently graduated in 2024 with a specialization in web development, focusing primarily on React.js. I am excited to embark on this journey as a professional React developer and share my experiences, insights, and projects with the tech community.</h4>
+
+The Journey So Far
+My path to becoming a React developer started during my early years in college when I was introduced to web development. The idea of creating dynamic, user-friendly web applications fascinated me, and I quickly gravitated towards front-end development. React.js, with its component-based architecture and efficiency, became my tool of choice.
+          </Text>
+        </Stack>
+      </Container>
+    </Box>
+  );
+};
+
+const MyCarousel = () => (
+  <Carousel
+    autoPlay
+    infiniteLoop
+    interval={1000}
+    showStatus={false}
+    showThumbs={false}
+    showArrows={false}
+  >
+    <Box w="full" h={'100vh'}>
+      <Image src={img1} h="full" w={'full'} objectFit={'cover'} />
+      <Heading bgColor={'blackAlpha.600'} color={'white'} {...headingOptions}>
+         The Future Is Here
+      </Heading>
+    </Box>
+    <Box w="full" h={'100vh'}>
+      <Image src={img2} h="full" w={'full'} objectFit={'cover'} />
+      <Heading bgColor={'whiteAlpha.900'} color={'black'} {...headingOptions}>
+        Future is Gaming
+      </Heading>
+    </Box>
+
+    <Box w="full" h={'100vh'}>
+      <Image src={img3} h="full" w={'full'} objectFit={'cover'} />
+      <Heading bgColor={'whiteAlpha.600'} color={'black'} {...headingOptions}>
+        Gaming on Console
+      </Heading>
+    </Box>
+
+    <Box w="full" h={'100vh'}>
+      <Image src={img4} h="full" w={'full'} objectFit={'cover'} />
+      <Heading bgColor={'whiteAlpha.600'} color={'black'} {...headingOptions}>
+        Night life is cool
+      </Heading>
+    </Box>
+  </Carousel>
+);
+
+export default Home;
